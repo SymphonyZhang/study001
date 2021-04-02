26538ae8f6a9b5473c7857a5d24e9c7558e214f9
SymphonyZhang <95200261@qq.com>
2021-04-02 17:29:55 +0800

    function
    函数的定义

--- pages/func/func.js
+++ pages/func/func.js
@@ -1,77 +1,20 @@
-// pages/func/func.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onLoad:function(){
+    console.log("function onLoad")
+    this.func1()
+    this.func2()
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    // 打印日志学习
-    console.log("onLoad")
-    console.log(1+1)
-    console.log("Hello Wrold")
-    console.log("Hello","Wrold")
+  func1(){
+    console.log("函数的第一种定义方式, 简写版")
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    // 打印日志学习
-    console.log("onReady")
+  func2:function(){
+    console.log("函数的第二种定义方式")
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    // 打印日志学习
-    console.log("onShow")
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    // 打印日志学习
-    console.log("onHide")
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    // 打印日志学习
-    console.log("onUnload")
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    // 打印日志学习
-    console.log("onPullDownRefresh")
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    // 打印日志学习
-    console.log("onReachBottom")
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    // 打印日志学习
-    console.log("onShareAppMessage")
-  }
+  // onLoad(){
+  //   console.log("onLoad")
+  // }
 })
