f1d75a1b9ebffa0a4ad01ff04a4bf1efd456a73b
SymphonyZhang <95200261@qq.com>
2021-04-02 17:12:31 +0800

    function
    函数，打印

---
+++ pages/func/func.js
@@ -0,0 +1,77 @@
+// pages/func/func.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 打印日志学习
+    console.log("onLoad")
+    console.log(1+1)
+    console.log("Hello Wrold")
+    console.log("Hello","Wrold")
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    // 打印日志学习
+    console.log("onReady")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 打印日志学习
+    console.log("onShow")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    // 打印日志学习
+    console.log("onHide")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    // 打印日志学习
+    console.log("onUnload")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    // 打印日志学习
+    console.log("onPullDownRefresh")
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    // 打印日志学习
+    console.log("onReachBottom")
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    // 打印日志学习
+    console.log("onShareAppMessage")
+  }
+})
